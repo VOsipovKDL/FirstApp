57db6aff5a6928a44407a11af18637b42717631b
VOsipov <VOsipov@kdl.ru>
2025-10-10 15:47:03 +0300

    learn data type

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,7 +6,15 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
         }
     }
 }
