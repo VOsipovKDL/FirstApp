de4b57d8014a18231e7d046b631256a2c742f828
VOsipov <VOsipov@kdl.ru>
2025-10-14 12:57:30 +0300

    sort 2 level list

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,17 +7,21 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
                 {
-                    if (arr[i]<=arr[j]) continue;
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    {
+                        if (arr[i, j]<=arr[i, k]) continue;
 
-                    int oldIndexValue = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = oldIndexValue;
+                        int oldIndexValue = arr[i, j];
+                        arr[i, j] = arr[i, k];
+                        arr[i, k] = oldIndexValue;
+                    }
+
                 }
                     
             }
