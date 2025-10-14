188ef3b5eb8a923c12f243611720c0753141584f
VOsipov <VOsipov@kdl.ru>
2025-10-14 16:46:32 +0300

    4 modul end

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,28 +7,33 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            (string name, string lastName, string login, byte loginLength, bool hasPet, byte age, string[] favcolor) User;
 
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            for (int k = 0; k < 3; k++)
             {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                Console.WriteLine("Ваше имя:");
+                User.name = Console.ReadLine();
+
+                Console.WriteLine("Ваша фамилия:");
+                User.lastName = Console.ReadLine();
+
+                Console.WriteLine("Придумайте логин:");
+                User.login = Console.ReadLine();
+                User.loginLength = (byte)User.login.Length;
+
+                Console.WriteLine("У вас есть животные?");
+                User.hasPet = (Console.ReadLine().ToUpper()=="ДА") ? true : false;
+
+                Console.WriteLine("Ваш возраст:");
+                User.age = (byte)Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Напишите три любимых цвета:");
+
+                User.favcolor = new string[3];
+                for (int i = 0; i< User.favcolor.Length; i++)
                 {
-                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
-                    {
-                        if (arr[i, j]<=arr[i, k]) continue;
-
-                        int oldIndexValue = arr[i, j];
-                        arr[i, j] = arr[i, k];
-                        arr[i, k] = oldIndexValue;
-                    }
-
+                    User.favcolor[i] = Console.ReadLine();
                 }
-                    
-            }
-
-            foreach (var item in arr)
-            {
-                Console.Write(item);
             }
         }
     }
