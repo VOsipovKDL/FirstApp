08be0f45723e61ece48d221cc8d2c7ce088711d0
VOsipov <VOsipov@kdl.ru>
2025-10-16 16:44:32 +0300

    5 modul end

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,8 +8,128 @@ namespace FirstApp
     {
         public static void Main(string[] args)
         {
-            string phrase = "Привет лес";
-            Echo(phrase, 5);
+            (string name, string lastName, byte age, bool hasPet, byte petCount, string[] petNameArray, byte favColorCount, string[] favColorArray) User;
+            GetUserInfo(out User);
+
+            Console.WriteLine("Вас зовут: {0} {1}, ваш возраст: {2}", User.name, User.lastName, User.age);
+            if (User.hasPet)
+            {
+                Console.WriteLine("У вас есть питомцы: {0} шт", User.petCount);
+                foreach (var item in User.petNameArray) Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Вы указали цвета, которые вам нравятся: {0} шт", User.favColorCount);
+            foreach (var item in User.favColorArray) Console.WriteLine(item);
+        }
+
+        static void GetUserInfo(out (string name, string lastName, byte age, bool hasPet, byte petCount, string[] petNameArray, byte favColorCount, string[] favColorArray) User)
+        {
+            Console.Write("Укажите ваше имя: ");
+            User.name = Console.ReadLine();
+
+            Console.Write("Укажите вашу фамилию: ");
+            User.lastName = Console.ReadLine();
+
+            User.age = (byte)GuarantedAnswerFromConsole("Укажите ваш возраст: ", false);
+            User.hasPet = GuarantedAnswerFromConsole("Есть ли у вас питомцы? Да/Нет: ");
+
+            User.petCount = 0;
+            User.petNameArray = new string[User.petCount];
+            if (User.hasPet) {
+                User.petCount = (byte)GuarantedAnswerFromConsole("Сколько у вас питомцев?: ", false);
+                User.petNameArray = GetPetNameArray(User.petCount);
+            }
+
+            User.favColorCount = 0;
+            User.favColorCount = (byte)GuarantedAnswerFromConsole("Сколько цветов вам нравится?: ", true);
+            User.favColorArray = GetFavColorArray(User.favColorCount);
+        }
+
+        static string[] GetPetNameArray(byte petCount)
+        {
+            string[] petNameArray = new string[petCount];
+
+            if (petCount==0) return petNameArray;
+
+            for (int i = 0; i<petNameArray.Length; i++)
+            {
+                Console.Write("Введите имя вашего питомца #{0}: ",i+1);
+                petNameArray[i] = Console.ReadLine();
+            }
+
+            return petNameArray;
+        }
+
+        static string[] GetFavColorArray(byte favColorCount)
+        {
+            string[] favColorArray = new string[favColorCount];
+
+            if (favColorCount==0) return favColorArray;
+
+            for (int i = 0; i<favColorArray.Length; i++)
+            {
+                Console.Write("Введите цвет #{0}: ", i+1);
+                favColorArray[i] = Console.ReadLine();
+            }
+
+            return favColorArray;
+        }
+
+        static bool GetIsCorrectNumberFromConsole(out int correctNumber)
+        {
+            return int.TryParse(Console.ReadLine(), out correctNumber);
+        }
+
+        static bool GuarantedAnswerFromConsole(string question)
+        {
+            bool answer;
+
+            while (true)
+            {
+                Console.Write(question);
+                string tempPet = Console.ReadLine().ToUpper();
+
+                if (tempPet=="ДА")
+                {
+                    answer = true;
+                    break;
+                }
+                else if (tempPet=="НЕТ")
+                {
+                    answer = false;
+                    break;
+                }
+            }
+
+            return answer;
+        }
+
+        static int GuarantedAnswerFromConsole(string question, bool hasZero)
+        {
+            int answer;
+
+            while (true)
+            {
+                Console.Write(question);
+                int temp;
+                bool isNumeric = GetIsCorrectNumberFromConsole(out temp);
+
+                if ((isNumeric)&&(temp>0))
+                {
+                    answer = temp;
+                    break;
+                }
+                else if ((isNumeric)&&(temp==0))
+                {
+                    if (hasZero)
+                    {
+                        answer = temp;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
         }
 
         static string GetColorAndShow(string userName, int age)
@@ -151,24 +271,44 @@ namespace FirstApp
         {
             var modif = phrase;
 
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine(modif);
 
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
+            if (modif.Length > 2) modif = modif.Remove(0, 2);
 
-            if (deep > 1)
+            if (deep > 1) Echo(modif, deep - 1);
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
             {
-                Echo(modif, deep - 1);
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
             }
         }
 
-        enum Semaphore : int
+        private static int PowerUp(int N, byte pow)
         {
-            Red = 100,
-            Yellow = 200,
-            Green = 300,
+            if (pow==0)
+            {
+                return 1;
+            }
+            else if (pow==1)
+            {
+                return N;
+            }
+
+            return N * PowerUp(N, --pow);
+        }
+
+        enum consoleAnswerType : int
+        {
+            Int,
+            Bool,
         }
     }
 }
